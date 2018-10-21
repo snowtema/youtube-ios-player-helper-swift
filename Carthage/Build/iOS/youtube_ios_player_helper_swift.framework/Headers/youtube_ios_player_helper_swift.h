fedfb3754e487f0a56a424f395e433b2ead0952b
Tema Snow <tema.slizhik@gmail.com>
2018-10-21 18:06:45 +0300

    support carthage

---
+++ Carthage/Build/iOS/youtube_ios_player_helper_swift.framework/Headers/youtube_ios_player_helper_swift.h
@@ -0,0 +1,19 @@
+//
+//  youtube_ios_player_helper_swift.h
+//  youtube-ios-player-helper-swift
+//
+//  Created by Bender Bad Robot on 21/10/2018.
+//  Copyright © 2018 Benderbadrobot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for youtube_ios_player_helper_swift.
+FOUNDATION_EXPORT double youtube_ios_player_helper_swiftVersionNumber;
+
+//! Project version string for youtube_ios_player_helper_swift.
+FOUNDATION_EXPORT const unsigned char youtube_ios_player_helper_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <youtube_ios_player_helper_swift/PublicHeader.h>
+
+
